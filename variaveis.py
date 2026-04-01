f568418c9254288598cca62344a6977180400a27
dayanecristine0112 <dayanecristineviana@gmail.com>
2026-04-01 13:36:50 -0300

    Create variaveis.py to request user input

---
+++ variaveis.py
@@ -0,0 +1,6 @@
+# variaveis.py
+
+# Script to request user input for city, year, and temperature
+cidade = input('Digite o nome da cidade: ')
+ano = input('Digite o ano atual: ')
+temperatura = input('Digite a temperatura média: ')
