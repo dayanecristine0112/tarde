94bcdc02a7b9f30217c17c11f6b63d8919fb7841
dayanecristine0112 <dayanecristineviana@gmail.com>
2026-04-01 14:37:03 -0300

    Add files via upload

---
+++ exe01.py
@@ -0,0 +1,16 @@
+# 1. Variável 'cidade' (texto)
+cidade = input("Digite o nome da sua cidade: ")
+
+# 2. Variável 'ano' (texto, pois não faremos contas com ele, ou int para número)
+ano = input("Digite o ano atual: ")
+
+# 3. Variável 'temperatura' (float para aceitar números decimais)
+temperatura = float(input("Digite a temperatura média da sua cidade (°C): "))
+
+# Exibindo os resultados para confirmar
+print("\n--- Dados Recebidos ---")
+print(f"Cidade: {cidade}")
+print(f"Ano: {ano}")
+print(f"Temperatura: {temperatura}°C")
+
+
