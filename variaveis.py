1fcc22b849f7aa14470ef5efd488574f83b01ce7
dayanecristine0112 <dayanecristineviana@gmail.com>
2026-04-01 14:00:20 -0300

    Delete variaveis.py

--- variaveis.py
+++
@@ -1,6 +0,0 @@
-# variaveis.py
-
-# Script to request user input for city, year, and temperature
-cidade = input('Digite o nome da cidade: ')
-ano = input('Digite o ano atual: ')
-temperatura = input('Digite a temperatura média: ')
